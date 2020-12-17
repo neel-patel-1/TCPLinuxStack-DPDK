6dd4f74a90d0f5e6b20d734e61511b65755db753
Neel Patel <neelpatelbiz@gmail.com>
2020-12-16 22:16:56 -0600

    added DNS lookup printout and socketbind example

--- Beej/systemcalls.c
+++ Beej/systemcalls.c
@@ -46,13 +46,64 @@ void clientConnect()
 	freeaddrinfo(servinfo);
 }
 
-void printIPAddresses()
+void printIPAddresses(int argc, char *argv[])
 {
 	struct addrinfo hints, *res, *p;
 	int status;
-}
-int main()
-{
+	char ipstr[INET6_ADDRSTRLEN];
 
+	if(argc !=2)
+	{
+		fprintf(stderr, "usage: showip hostname\n");
+		return 1
+	}
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if( status = getaddrinfo(argv[1], NULL, &hints, &res) != 0)
+	{
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+	}
+	printf("IP addresses for %s:\n\n", argv[1]);
+	for(p = res; p!=NULL; p=p->ai_next)
+	{
+		void *addr;
+		char *ipver;
+
+		if(p->ai_family == AF_INET)
+		{
+			struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+			addr = &(ipv4->sin_addr);
+			ipver = "IPv4";
+		}
+		else
+		{
+			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+			addr = &(ipv6->sin6_addr);
+			ipver = "IPv6";
+		}
+		inet_ntop(p->aifamily, addr, ipstr, sizeof ipstr);
+		printf(" %s: %s\n", ipver, ipstr);
+	}
+	freeaddrinfo(res);
+}
+void bindSocket()
+{
+	struct addrinfo hints, *res;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE; //auto-fill IP
+	getaddrinfo(NULL, "3490", &hints, *res);// socket to be bound to port 3490
+	
+	int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+	bind(sockfd, res->ai_addr, res->ai_addrlen);
+}
+int main(int argc, char *argv[])
+{
+	printIPAddresses(argc, argv);
 	
 }
