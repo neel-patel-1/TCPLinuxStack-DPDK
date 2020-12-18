05013ce22be1e5a0781eca17ef5bce90a9b2a6c4
Neel Patel <neelpatelbiz@gmail.com>
2020-12-18 12:29:34 -0600

    testing timer

--- client/client.c
+++ client/client.c
@@ -7,56 +7,20 @@
 
 #include <errno.h>
 #include <string.h>
- 
-/*
-int main()
-{
-	char serverIPAddress[15] = "127.000.000.001";
-	//create a socket
-	int network_socket;
-	network_socket = socket(AF_INET, SOCK_STREAM, 0);	
-	
-	//connect to another socket (using connect function)
-	//first specify an address to connect to
-	struct sockaddr_in server_address;
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(9002);
-	if(inet_pton(AF_INET, serverIPAddress, &(server_address.sin_addr))<0)
-	{
-		printf("Server address could not be resolved\n");
-		return 1;
-	}
-	//value of 0 is success, -1 is a fail
-	int connection = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+#include <time.h>
 
-	if(connection == -1)
-	{
-		printf("Connection failed with errno: %d\n", errno);
-		return 2;
-	}
-
-	char server_response[256];	
-	recv(network_socket, &server_response, sizeof(server_response), 0);
-
-	printf("The server sent the data: %s \n", server_response);
-	
-	//close the socket
-	close(network_socket);
-	return 0;
-}*/
-//pass in args from command line for benchmark
-//server should accept incoming connections and send some kind
-//of information back to the client
-//client should measure latency
 #define MSG_LENGTH 28
 #define SERVER_IPv4 "127.0.0.1"
 #define SERVER_IPv6 "::1"
 #define SERVER_PORT "3490"
 #define ITERATIONS 5
+
+timespec diff(timespec start, timespec end);
 int main()
 {
 	int status, sockfd, numBytes;
 	struct addrinfo hints, *res;
+	struct timespec before, after, difference;
 	char server_response[MSG_LENGTH];
 
 	memset(&hints, 0, sizeof hints);
@@ -78,22 +42,38 @@ int main()
 	printf("Testing...\n");
 	for(int i=0; i<ITERATIONS; i++)
 	{
+		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &before);
 		if (connect(sockfd, res->ai_addr, res->ai_addrlen) == -1)
 		{
 			fprintf(stderr, "connect: %s\n ", strerror(errno));
 			return 2;
 		}
 		numBytes = recv(sockfd, &server_response, sizeof(server_response), 0);
-
+		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &after);
 		if( numBytes == -1 )
 		{
 			fprintf(stderr, "recv: %s\n", strerror(errno));
 		}
 		else 
 		{
-			printf("Latency: %s\n", server_response);
+			difference = difftime(before, after);
+			printf("Latency (server-side): %s , RTT: %d:%ld\n", server_response, difference.tv_sec, difference.tv_nsec);
 		}
 	}
 
 	close(sockfd);
 }
+timespec diff(timespec start, timespec end)
+{
+    timespec temp;
+    if ((end.tv_nsec-start.tv_nsec)<0) {
+        temp.tv_sec = end.tv_sec-start.tv_sec-1;
+        temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
+    } else {
+        temp.tv_sec = end.tv_sec-start.tv_sec;
+        temp.tv_nsec = end.tv_nsec-start.tv_nsec;
+    }
+    return temp;
+}
+//use CLOCK_PROCESS_CPUTIME_ID
+//
